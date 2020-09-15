85731c429160adda3bfbdcdb9d468a362a8192c0
ushills <web@ushills.co.uk>
2020-09-15 15:32:07 +0000

    clean up and basic flask template

--- xox/game.py
+++ xox/game.py
@@ -1,11 +1,16 @@
-from flask import session, redirect, url_for, request, render_template
+import os
+from flask import Flask, session, redirect, url_for, request, render_template
+import xox.services.xox as xox
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY') or \
+    'Not_very_secret'
 
 @app.route('/')
 def game():
-    session['answer'] = random.randint(1, 10)
-    session['try_number'] = 1
-    return redirect(url_for('play'))
-    return 'Hello from Flask!'
+    game = xox.XOXGame()
+    session['board'] = game.board
+    # session['try_number'] = 1
+    # return redirect(url_for('play'))
+    return str(session['board'])
 

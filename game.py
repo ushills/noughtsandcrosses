24a6c79ba90bd3df66aae898d751f67ca720e940
ushills <web@ushills.co.uk>
2020-09-16 12:57:04 +0100

    buttons created with range

--- game.py
+++ game.py
@@ -1,6 +1,7 @@
 import tkinter as tk
 import tkinter.font as font
 
+
 class Application(tk.Frame):
     def __init__(self, master=None):
         super().__init__(master)
@@ -9,24 +10,26 @@ class Application(tk.Frame):
         self.create_buttons()
 
     def create_buttons(self):
-#         self.hi_there = tk.Button(self)
-#         self.hi_there["text"] = "Hello World\n(click me)"
-#         self.hi_there["command"] = self.say_hi
-#         self.hi_there.pack(side="top")
-# 
-#         self.quit = tk.Button(self, text="QUIT", fg="red",
-#                               command=self.master.destroy)
-#         self.quit.pack(side="bottom")
-
-        self.button_1 = tk.Button(self)
-        self.button_1["command"] = lambda: self.move(1)
-        self.button_1.pack()
-        self.button_2 = tk.Button(self)
-        self.button_2["command"] = lambda: self.move(2)
-        self.button_2.pack()
+        #         self.hi_there = tk.Button(self)
+        #         self.hi_there["text"] = "Hello World\n(click me)"
+        #         self.hi_there["command"] = self.say_hi
+        #         self.hi_there.pack(side="top")
+        #
+        #         self.quit = tk.Button(self, text="QUIT", fg="red",
+        #                               command=self.master.destroy)
+        #         self.quit.pack(side="bottom")
+        self.button = []
+        for i in range(9):
+            self.button.append(
+                tk.Button(
+                    self, text=str(i + 1), command=lambda i=i+1: self.move(i)
+                )
+            )
+            self.button[i].grid(column=4, row=i + 1)
 
     def move(self, square_number):
         print("You pressed", square_number)
+
 
 root = tk.Tk()
 app = Application(master=root)
